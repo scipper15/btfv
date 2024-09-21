446830f06d21078aeeaddc6c5a379c76e3773094
scipper15 <reinhard.eichhorn@gmail.com>
2024-09-21 17:51:13 +0200

    feat(config): Added settings using `Pydantic`
    Updated dependencies, created ´Settings()´ class as a new shared package, updated ´main.py´ and ´Scraper()´ to use settings

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,5 +1,6 @@
 class Scraper:
-    def __init__(self) -> None:
+    def __init__(self, settings) -> None:
+        self.settings = settings
         print(f"Hello from {Scraper.__name__}!")
 
     def scrape(self):
