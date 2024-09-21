ff06a68b72db72c63f9daaad55d73866a80e094d
scipper15 <reinhard.eichhorn@gmail.com>
2024-09-21 20:13:08 +0200

    feat(scraper): Added ´RAW_HTML_PATH´ to ´settings.py´, using value in ´scraper.py´

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,7 +1,16 @@
+import typing
+
+if typing.TYPE_CHECKING:
+    from logging import Logger
+
+    from shared.config.settings import Settings
+
+
 class Scraper:
-    def __init__(self, settings, logger) -> None:
+    def __init__(self, settings: "Settings", logger: "Logger") -> None:
         self.settings = settings
         self.logger = logger
+        self.RAW_HTML_PATH = self.settings.RAW_HTML_PATH
         logger.info(f"Hello from {Scraper.__name__}!")
 
     def scrape(self) -> None:
--- src/shared/config/settings.py
+++ src/shared/config/settings.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -5,7 +8,19 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=(".env.dev", ".env.test", ".env.prod"))
     PYTHONDONTWRITEBYTECODE: bool = False
     PYTHONPATH: str = "./src"
+
     APPNAME: str = "btfv.tablesoccer.rocks"
+
+    RAW_HTML_PATH: Path = Field(default=Path.cwd() / "data" / "raw_html")
+
+    @field_validator("RAW_HTML_PATH", mode="before")
+    def convert_raw_html_path(cls, RAW_HTML_PATH):
+        if isinstance(RAW_HTML_PATH, str):
+            RAW_HTML_PATH = Path.cwd() / RAW_HTML_PATH
+            Path.mkdir(RAW_HTML_PATH, exist_ok=True, parents=True)
+            return RAW_HTML_PATH
+        Path.mkdir(RAW_HTML_PATH, exist_ok=True, parents=True)
+        return RAW_HTML_PATH
 
 
 settings = Settings()
