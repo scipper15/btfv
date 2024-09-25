cb4343f830e5470d9ada20910acd0920f11bb386
scipper15 <reinhard.eichhorn@gmail.com>
2024-09-25 11:49:43 +0200

    chore(environment): Added new environment variables for scraper and database

--- src/shared/config/settings.py
+++ src/shared/config/settings.py
@@ -6,18 +6,32 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=(".env.dev", ".env.test", ".env.prod"))
+
     PYTHONDONTWRITEBYTECODE: bool = False
     PYTHONPATH: str = "./src"
-
-    APPNAME: str = "btfv.tablesoccer.rocks"
     LOGGING_ENV: str = Field(default="dev")
 
+    APPNAME: str = "btfv.tablesoccer.rocks"
+
+    # scraper
+    SCRAPER_INTERVAL: int = Field(default=86400)
     BASE_PATH: Path = Field(default=Path.cwd())
     RAW_HTML_PATH: Path = Field(default=Path.cwd() / "data" / "raw_html")
     PLAYER_HTML_PATH: Path = Field(default=Path.cwd() / "data" / "player_html")
 
     BTFV_URL_BASE: str = "https://btfv.de/sportdirector"
     DTFB_URL_BASE: str = "https://dtfb.de/wettbewerbe/turnierserie/spielersuche"
+
+    # database
+    POSTGRES_USER: str = Field(default="docker")
+    POSTGRES_PASSWORD: str = Field(default="docker")
+    POSTGRES_DB: str = Field(default="db")
+    SYNC_URL: str = Field(
+        default="postgresql+psycopg2-binary://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/${POSTGRES_DB}"
+    )
+    ASYNC_URL: str = Field(
+        default="postgresql+asyncpg://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/${POSTGRES_DB}"
+    )
 
     @field_validator("RAW_HTML_PATH", mode="before")
     def convert_raw_html_path(cls, RAW_HTML_PATH):
